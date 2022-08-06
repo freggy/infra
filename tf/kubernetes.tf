1899ac12c31a683ce006a187b9e7899a5cedcd14
Yannic Rieger <yannic.rieger@hetzner-cloud.de>
2022-08-06 14:40:22 +0200

    change ssh key to saturn-mgmt and fix health check intervals

--- tf/kubernetes.tf
+++ tf/kubernetes.tf
@@ -1,5 +1,5 @@
 resource "hcloud_load_balancer" "saturn-control-plane-lb" {
-  name = "saturn-api-server"
+  name = "saturn-api-servers"
   load_balancer_type = "lb11"
   location = "fsn1"
   labels = {
@@ -10,7 +10,7 @@ resource "hcloud_load_balancer" "saturn-control-plane-lb" {
 resource "hcloud_load_balancer_target" "saturn-control-plane-lb-target" {
   type = "label_selector"
   load_balancer_id = hcloud_load_balancer.saturn-control-plane-lb.id
-  label_selector = "cluster=saturn"
+  label_selector = "saturn-master"
 }
 
 resource "hcloud_load_balancer_service" "saturn-control-plane-lb-service" {
@@ -21,25 +21,23 @@ resource "hcloud_load_balancer_service" "saturn-control-plane-lb-service" {
   health_check {
     protocol = "tcp"
     port = 443
-    interval = 10
-    timeout = 30
+    interval = 30
+    timeout = 10
     retries = 3
   }
 }
 
-resource "hloud_server" "saturn-masters" {
+resource "hcloud_server" "saturn-masters" {
   count = 2
   name = "master${count.index + 1}-saturn-k8s-freggy-dev"
   image = "debian-11"
   server_type = "cpx11"
   location = "fsn1"
-  ssh_keys = ["management-key"]
-  public_net {
-    ipv4_enabled = false
-  }
+  ssh_keys = ["saturn-mgmt"]
   labels = {
     roles = "saturn.kube_master"
     cluster = "saturn"
+    saturn-master = "true"
   }
 }
 
@@ -48,7 +46,7 @@ resource "hcloud_server" "saturn-workers" {
   name = "worker${count.index + 1}-saturn-k8s-freggy-dev"
   image = "debian-11"
   server_type = "cx21"
-  ssh_keys = ["management-key"]
+  ssh_keys = ["saturn-mgmt"]
   location = "fsn1"
   labels = {
     roles = "saturn.kube_worker"
