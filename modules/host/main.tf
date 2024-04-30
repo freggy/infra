46b14ec3eed98e3a5468bea77c231e34aecfb346
Yannic Rieger <riegeryannic@gmail.com>
2024-04-30 23:54:01 +0200

    add server name and labels to host module

--- modules/host/main.tf
+++ modules/host/main.tf
@@ -1,10 +1,11 @@
 resource "hcloud_server" "server" {
   count       = var.is_hcloud_server ? 1 : 0
-  name        = var.hcloud_name
+  name        = var.name
   image       = var.hcloud_image
   server_type = var.hcloud_server_type
   location    = var.hcloud_location
   ssh_keys    = var.hcloud_ssh_keys
+  labels      = var.hcloud_labels
 }
 
 terraform {
--- modules/host/variables.tf
+++ modules/host/variables.tf
@@ -1,3 +1,7 @@
+variable "name" {
+  type = string
+}
+
 /*
  * dedicated server properties
  */
@@ -26,22 +30,22 @@ variable "hcloud_server_type" {
   default = ""
 }
 
-variable "hcloud_name" {
-  type = string
-  default = ""
-}
-
 variable "hcloud_location" {
   type = string
   default = "fsn1"
 }
 
 variable "hcloud_image" {
-  type = string
+  type = any
   default = "ubuntu-22.04"
 }
 
 variable "hcloud_ssh_keys" {
   type = list(string)
   default = []
+}
+
+variable "hcloud_labels" {
+  type = map(string)
+  default = {}
 }
