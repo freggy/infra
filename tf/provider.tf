2e6fad8284ff8317456fb63211207323d7a8e0c8
yannic rieger <yannic.rieger@hetzner-cloud.de>
2024-10-07 22:30:21 +0200

    update cluster configuration

--- tf/provider.tf
+++ tf/provider.tf
@@ -8,12 +8,18 @@ terraform {
       source  = "tailscale/tailscale"
       version = ">= 0.17.1"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "5.0.0-alpha1"
+    }
   }
 }
 
 variable "hcloud_token" {}
 variable "tailscale_token" {}
 variable "tailscale_tailnet" {}
+variable "cloudflare_token" {}
+variable "cloudflare_account_id" {}
 
 provider "hcloud" {
   token = var.hcloud_token
@@ -22,4 +28,8 @@ provider "hcloud" {
 provider "tailscale" {
   api_key = var.tailscale_token
   tailnet = var.tailscale_tailnet
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_token
 }
